c6437eac6fe570b55964674e5c3161e05783341c
Arun Pandy <arunpandy12345@gmail.com>
2026-09-09 21:28:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,29 +2,29 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const env = process.argv[2] || "dev";
-dotenv.config ({ path: `.env.${env}` });
+dotenv.config({ path: `.env.${env}` });
 
 function log(message) {
-	const time = new Date().toISOString();
-	console.log(`[${time}] [${env.toUpperCase()}] ${message}`);
+    const time = new Date().toISOString();
+    console.log(`[${time}] [${env.toUpperCase()}] ${message}`);
 }
 
 const app = express();
 
-const PORT = process.env.PORT
-const APP = process.env.APP
+const PORT = process.env.PORT || 3000;
+const APP = process.env.APP || "Development";
 
 app.get('/', (req, res) => {
     res.send("Hello from express from " + APP);
 });
 
 app.get('/health', (req, res) => {
-	log ("Health Check Called");
-	res.status(200).json({
-		status: "UP"
-	});
+    log("Health Check Called");
+    res.status(200).json({
+        status: "UP"
+    });
 });
 
-app.listen(PORT , () => {
-	log("servere is running on port " + PORT);
+app.listen(PORT, () => {
+    log("server is running on port " + PORT);
 });
